493674e9da388d12407a706fd6024671c5a2f816
Gustavo Canul <gusssy12@gmail.com>
2024-04-22 00:02:28 -0600

    xd

--- lib/presentation/screens/widgets/shared/message_field_box.dart
+++ lib/presentation/screens/widgets/shared/message_field_box.dart
@@ -8,23 +8,23 @@ class MessageFieldBox extends StatelessWidget {
   Widget build(BuildContext context) {
     // final colors = Theme.of(context).colorScheme;
 
-    final textController = TextEditingController();
+    final textController = TextEditingController(); //*Le agregamos un controlador al TextFormField
 
-    final focuesNode = FocusNode();
+    final focuesNode = FocusNode(); //*Es para poder hacer acciones de mas. 
 
     final UnderlineInputBorder outlineInputBorder = UnderlineInputBorder(
           borderSide: const BorderSide( color: Colors.white ),
           borderRadius: BorderRadius.circular(25),
         );
 
-    final InputDecoration inputDecoration = InputDecoration(
+    final InputDecoration inputDecoration = InputDecoration( //*Agregamos el input decoracion
         enabledBorder: outlineInputBorder,
         focusedBorder: outlineInputBorder,
         contentPadding: const EdgeInsets.symmetric(vertical: 12, horizontal: 10),
         hintText: 'End your message with a "??"',
         filled: true,
 
-        suffixIcon: IconButton(
+        suffixIcon: IconButton( //*este es el icon que parace en la parte derecha del textformfield
           onPressed: () {
             //*Aqui ya es el mensaje que enviaremos, cuando apretemos el icono de enviar, pero aún falta terminar la logica de esta parte del codigo
             final textValue = textController.value.text;
@@ -32,18 +32,18 @@ class MessageFieldBox extends StatelessWidget {
             textController.clear();
           },
           icon: const Icon(Icons.send)
-          ),
+        ),
       );
 
     return TextFormField(
       onTapOutside: (event) {
-        focuesNode.unfocus();
+        focuesNode.unfocus(); //*Esto es para que se cierre el form si toco afuera de la pantala
       },
       focusNode: focuesNode,
-      controller: textController,
-      decoration: inputDecoration,
+      controller: textController,//*Agregamos el controlador que creamos
+      decoration: inputDecoration, //*Es para decorar el TextFormField
       //*Con esta propiedad de TexFormFielda agregamos el sumit del valor cuado le damos a la flecha de terminar de escribir(Sumit)
-      onFieldSubmitted: (value) {
+      onFieldSubmitted: (value) { //! Esto es lo que se ejecuta cuando manamos el valor.
         onValue(textController.value.text);
         textController.clear();
         focuesNode.requestFocus();
